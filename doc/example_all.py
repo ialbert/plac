4088817397ecd58a96503fd1c5b281401ad900f3
Michele Simionato <michele.simionato@gmail.com>
2020-06-05 10:27:10 +0200

    Fixed test

--- doc/example_all.py
+++ doc/example_all.py
@@ -1,9 +1,12 @@
 import plac
-import pathlib
+try:
+    from pathlib import Path
+except ImportError:  # in Python 2.7
+    Path = str
 
 
 @plac.pos('model', "Model name", choices=['A', 'B', 'C'])
-@plac.opt('output_dir', "Optional output directory", type=pathlib.Path)
+@plac.opt('output_dir', "Optional output directory", type=Path)
 @plac.opt('n_iter', "Number of training iterations", type=int)
 @plac.flg('debug', "Enable debug mode")
 def main(model, output_dir='.', n_iter=100, debug=False):
